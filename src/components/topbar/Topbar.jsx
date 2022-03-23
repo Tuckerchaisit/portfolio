d1433fdfc0f0b7d04dcea59f8fbdb569f55c28cc
Tucker <tucker.chaisit@gmail.com>
2022-03-22 22:19:42 -0400

    implement material UI

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,6 @@
 import "./topbar.scss"
+import MailIcon from '@mui/icons-material/Mail';
+import PersonIcon from '@mui/icons-material/Person';
 
 export default function Topbar() {
   return (
@@ -7,12 +9,20 @@ export default function Topbar() {
           <div className="left">
             <a href="#intro" className="logo">tucker.</a>
             <div className="itemContainer">
-              
+              <PersonIcon className="icon"/>
+              <span>+44 924</span>
+            </div>
+            <div className="itemContainer">
+              <MailIcon className="icon"/>
+              <span>+safak@genius.com</span>
             </div>
           </div>
-          <h1>Hello</h1>
           <div className="right">
-            this is right
+              <div className="hamburger">
+                <span className="line1"></span>
+                <span className="line2"></span>
+                <span className="line3"></span>
+              </div>
           </div>
         </div>
     </div>
